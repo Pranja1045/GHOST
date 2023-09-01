6eb54196201f8932f1c5ec9f44f4f7a4ba2a4ed4
Pranshu Raj <rajpranshu2222@gmail.com>
2023-09-01 15:21:09 +0530

    updated code with readme

--- server.py
+++ server.py
@@ -1,9 +1,8 @@
 import socket
-import logging
 from bitstring import ConstBitStream
-from time import time
 from threading import Thread
 from coords import *
+
 
 class Server:
 
@@ -20,15 +19,13 @@ class Server:
         host = socket.gethostname()
         self.socket_servidor.bind(("localhost", 10001))
         self.socket_servidor.listen(5)
-        logging.info("Servidor escuchando en {}:{}...".format(
-            "localhost", 10001))
 
     def accept_connections(self):
         thread = Thread(target=self.accept_connections_thread)
         thread.start()
 
     def accept_connections_thread(self):
-        logging.info("Servidor aceptando conexiones...")
+
         while True:
             client_socket, _ = self.socket_servidor.accept()
             self.socket_cliente = client_socket
@@ -37,7 +34,6 @@ class Server:
             listening_client_thread.start()
 
     def listen_client_thread(self, client_socket):
-        logging.info("Servidor conectado a un nuevo cliente...")
 
         while self.conectado:
             data0 = client_socket.recv(160)
@@ -59,9 +55,10 @@ class Server:
                 dec = data.read('hex:32')
                 data.bitpos = ant_pos
                 dec_int = data.read('intle:32')
-                stell_pos=eCoords2str(ra_uint, dec_int, mtime)
+                stell_pos = eCoords2str(ra_uint, dec_int, mtime)
 
                 print(f"Received stellar object position: {stell_pos}")
+
 
 if __name__ == "__main__":
     try:
